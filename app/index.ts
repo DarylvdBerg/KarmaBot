6adcdc7477b9bb12ff2ea93cc0fb24b7725c29a1
DarylvdBerg <darylvdberg@gmail.com>
2022-01-26 21:55:21 +0100

    removed discord helper

--- app/index.ts
+++ app/index.ts
@@ -1,41 +1,44 @@
 // import KarmaDao from "./database/karmaDao";
-import DiscordClient from "./helpers/discordclient";
-const Discord = require("discord.js");
+const { Client, Intents, Discord } = require("discord.js");
 
 const fs = require("fs");
 const path = require("path");
 const { token } = require("../config.json");
 
-const client = DiscordClient.getInstance().get(); 
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 client.commandPrefix = "k!";
 client.commands = new Discord.Collection();
 
 // const karmaDao = new KarmaDao();
 
 const commandDir = path.join(__dirname, "./commands");
-const commandFiles = fs.readdirSync(commandDir).filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync(commandDir)
+  .filter((file) => file.endsWith(".js"));
 
-for(const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
 }
 
-client.once('ready', () => {
-    // karmaDao.create();
-    console.log('Bot is ready');
+client.once("ready", () => {
+  // karmaDao.create();
+  console.log("Bot is ready");
 });
 
-client.on('messageCreate', message => {
-    if(!message.content.startsWith(client.commandPrefix)) return;
+client.on("messageCreate", (message) => {
+  if (!message.content.startsWith(client.commandPrefix)) return;
 
-    const args = message.content.slice(client.commandPrefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+  const args = message.content.slice(client.commandPrefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
 
-    try {
-        client.commands.get(command).execute(message, args);
-    } catch(error) {
-        message.channel.send("Karma is a bitch.");
-    }
+  try {
+    client.commands.get(command).execute(message, args);
+  } catch (error) {
+    message.channel.send("Karma is a bitch.");
+  }
 });
 
 client.login(token);
